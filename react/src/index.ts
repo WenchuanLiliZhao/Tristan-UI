95e757cfc7f504984ac8c1e33358e18e786f8603
Wenchuan Zhao <111623150+WenchuanLiliZhao@users.noreply.github.com>
2025-07-02 15:14:08 +0800

    1

--- react/src/index.ts
+++ react/src/index.ts
@@ -1,6 +1,6 @@
 // Tristan UI Design System - 完整的设计系统导出
 export * from './design-system/ui-components';
-export * from './design-system/ui-demos';
+// export * from './design-system/ui-demos';
 export * from './styles';
 
 // 引入全局样式
