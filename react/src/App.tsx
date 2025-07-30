e6673f8d68eaab86d64345372ec30541beaed6c6
Wenchuan Zhao <111623150+WenchuanLiliZhao@users.noreply.github.com>
2025-07-30 10:39:21 +0800

    1

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -5,10 +5,6 @@ function App() {
 
   return (
     <BrowserRouter
-      future={{
-        v7_startTransition: true,
-        v7_relativeSplatPath: true
-      }}
     >
       <Routes>
         <Route path="/" element={DemoPages.Home.content} />
